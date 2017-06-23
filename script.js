166ac21344eeaf8714cd1af451eb75bae01b5999
bbryant1185 <bbryant1185@yahoo.com>
2017-06-23 15:27:35 -0400

    functional with 3 actions

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 var $textInput = $("#textInput"),
     $button = $("#doAction"),
-    $display = $("#display");
+    $display = $("#display"),
+    $options = $("input[type=radio]"),
+    method = "reverse";
 
-$button.on("click", function(){
-  let word = $textInput.val();
+$options.on("click", function(){
+  method = $(this).val();
+});
+
+$button.on("click", function() {
   
-  let reversedWord = word.split("").reverse().join("");
+  if (method === "reverse") {
+    let word = $textInput.val();
+    let reversedWord = word.split("").reverse().join("");  
+    $display.text(reversedWord);  
+  }
   
-$display.text(reversedWord);
+  if (method === "random") {
+    let word = $textInput.val();
+    let randomizedWord = word.split("").sort(function(a, b) {
+      return 0.5 - Math.random();
+    });
+    $display.text(randomizedWord.join(""));
+  }
+  
+  if (method === "alphabetize") {
+    let word = $textInput.val();
+    let alphabetizedWord = word.split("").sort().join("");  
+    $display.text(alphabetizedWord);  
+  }
+  
 });
