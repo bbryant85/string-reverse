58b07b36f0a8b2405eeaa1e2b46c113c48769d9f
bbryant1185 <bbryant1185@yahoo.com>
2017-06-23 14:14:32 -0400

    2nd commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var $textInput = $("#textInput"),
+    $button = $("#doAction"),
+    $display = $("#display");
+
+$button.on("click", function(){
+  let word = $textInput.val();
+  
+  let reversedWord = word.split("").reverse().join("");
+  
+$display.text(reversedWord);
+});
